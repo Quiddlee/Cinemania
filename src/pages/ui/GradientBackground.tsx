c0833ce068263edf11294ddd82d2c3702c9f47f8
Bogdan <thegarrybit@gmail.com>
2023-10-26 15:36:05 +0300

    refactor: change background

--- src/pages/ui/GradientBackground.tsx
+++ src/pages/ui/GradientBackground.tsx
@@ -4,11 +4,12 @@ class GradientBackground extends Component {
   render() {
     return (
       <>
-        <div className="absolute z-10 h-full w-full bg-black bg-opacity-50 backdrop-blur-[170px]" />
+        <div className="absolute z-10 h-full w-full bg-black bg-opacity-50 backdrop-blur-[170px] backdrop-saturate-150" />
 
-        <div className="absolute right-72 top-0 h-1/2 w-1/3 rounded-full bg-pink-400" />
-        <div className="absolute left-96 top-0 h-1/2 w-1/3 rounded-full bg-violet-500 saturate-200" />
-        <div className="absolute top-2/4 h-1/4 w-1/6 rounded-full bg-cyan-700" />
+        <div className="absolute right-52 top-0 h-1/3 w-1/2 rounded-full bg-pink-300" />
+        <div className="absolute left-96 top-0 h-1/4 w-1/3 rounded-full bg-violet-400 saturate-200" />
+        <div className="absolute left-44 top-2/4 h-1/4 w-1/5 rounded-full bg-blue-400 saturate-200" />
+        <div className="absolute bottom-1/4 left-1/2 h-1/5 w-1/5 rounded-full bg-emerald-300" />
         <div className="absolute -right-32 bottom-0 h-1/3 w-1/2 rounded-full bg-emerald-800" />
       </>
     );
