856586d5106884c2b8b0fb7b24848085057136da
Bogdan <thegarrybit@gmail.com>
2023-10-27 17:01:55 +0300

    feat: add nice movie hover effect

--- src/entities/movie/ui/Movie.tsx
+++ src/entities/movie/ui/Movie.tsx
@@ -1,4 +1,6 @@
-import { Component } from 'react';
+import { Component, createRef, MouseEvent } from 'react';
+
+import colors from 'tailwindcss/colors';
 
 import ReactLogo from '../../../assets/reactJS-logo.png';
 import { NOT_EXIST } from '../../../shared/const/const.ts';
@@ -20,6 +22,8 @@ class Movie extends Component<IMovieProps, IMovieState> {
     genre: '',
   };
 
+  movieRef = createRef<HTMLDivElement>();
+
   async componentDidMount() {
     const movieData = await getMovie(this.props.data.imdbID);
     const genre = movieData.Genre.split(', ').slice(0, 2).join('/');
@@ -31,6 +35,22 @@ class Movie extends Component<IMovieProps, IMovieState> {
     });
   }
 
+  handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    const rect = this.movieRef.current?.getBoundingClientRect();
+
+    if (rect) {
+      const pointerX = e.clientX - rect.left;
+      const pointerY = e.clientY - rect.top;
+
+      if (this.movieRef.current)
+        this.movieRef.current.style.background = `radial-gradient(circle at ${pointerX}px ${pointerY}px, ${colors.pink['950']} 0%, ${colors.neutral['950']} 160px)`;
+    }
+  };
+
+  handleMouseOut = () => {
+    if (this.movieRef.current) this.movieRef.current.style.background = '';
+  };
+
   render() {
     const { Poster, Title, Year } = this.props.data;
     const { description, genre } = this.state;
@@ -39,20 +59,27 @@ class Movie extends Component<IMovieProps, IMovieState> {
     const poster = isPosterExist ? Poster : ReactLogo;
 
     return (
-      <li className="shadow-pink-6100 w-64 cursor-pointer space-y-4 rounded-[40px] bg-neutral-950 p-2 text-gray-100 shadow-sm transition-all duration-200 hover:bg-neutral-900">
-        <img
-          className="h-80 w-full rounded-[32px] object-cover"
-          src={poster}
-          alt={`The poster of ${Title} film`}
-        />
-        <article className="h-full p-4">
-          <h2 className="truncate text-xl text-gray-100">{Title}</h2>
-          <div className="mt-1 text-gray-400">
-            <p>{description}</p>
-            <span>{genre}</span>
-            <span>{Year}</span>
-          </div>
-        </article>
+      <li className="w-64 cursor-pointer overflow-hidden rounded-[40px] bg-neutral-950 text-gray-100 transition-all duration-200">
+        <div
+          ref={this.movieRef}
+          onMouseMove={this.handleMouseMove}
+          onMouseOut={this.handleMouseOut}
+          onBlur={this.handleMouseOut}
+          className="h-full space-y-4 rounded-[32px] p-2">
+          <img
+            className="h-80 w-full rounded-[32px] object-cover"
+            src={poster}
+            alt={`The poster of ${Title} film`}
+          />
+          <article className="h-full p-4">
+            <h2 className="truncate text-xl text-gray-100">{Title}</h2>
+            <div className="mt-1 grid text-gray-400">
+              <p>{description}</p>
+              <span>{genre}</span>
+              <span>{Year}</span>
+            </div>
+          </article>
+        </div>
       </li>
     );
   }
