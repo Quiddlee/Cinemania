2548eb9fbe4d346871e533d80873bed30e77563d
Bogdan <thegarrybit@gmail.com>
2023-10-28 22:29:09 +0300

    refactor: change fall query to "all"

--- src/shared/const/const.ts
+++ src/shared/const/const.ts
@@ -1,6 +1,6 @@
 export const API_KEY = 'dbb72d83';
 export const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}`;
-export const QUERY_FALLBACK = 'shazam';
+export const QUERY_FALLBACK = 'all';
 export const NOT_EXIST = 'N/A';
 export const LOCAL_STORAGE_SEARCH_QUERY = 'search-query';
 export const APP_TITLE = 'CINEMANIA';
