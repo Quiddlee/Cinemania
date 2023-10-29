3e995d0b2336efdf81d53497fdc13aba6602b552
Bogdan <thegarrybit@gmail.com>
2023-10-29 18:03:02 +0200

    refactor: change initial load animation

--- src/widgets/Header/ui/TotalResults.tsx
+++ src/widgets/Header/ui/TotalResults.tsx
@@ -12,7 +12,7 @@ class TotalResults extends Component {
 
   render() {
     return (
-      <p className="animation-delay ml-auto hidden animate-fade-in text-right text-lime-400 sm:block lg:text-lg">
+      <p className="ml-auto hidden animate-fade-in text-right text-lime-400 sm:block lg:text-lg">
         Found <span className="font-semibold">{this.context.totalResults}</span>{' '}
         movies
       </p>
