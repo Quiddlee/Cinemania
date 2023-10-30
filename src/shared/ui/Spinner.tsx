883dae950772d137bbab02bbd13f9d1cc4132cc5
Bogdan <thegarrybit@gmail.com>
2023-10-30 18:06:44 +0200

    refactor: change spinner to functional components

--- src/shared/ui/Spinner.tsx
+++ src/shared/ui/Spinner.tsx
@@ -1,27 +1,18 @@
-import { Component } from 'react';
-
 import { createPortal } from 'react-dom';
 
-import {
-  ISearchContext,
-  SearchContext,
-} from '../../features/Search/context/SearchProvider.tsx';
+import { useSearch } from '../../features/Search/context/SearchProvider.tsx';
 
-class Spinner extends Component {
-  static contextType = SearchContext;
+function Spinner() {
+  const { isLoading } = useSearch();
 
-  declare context: ISearchContext;
+  if (!isLoading) return null;
 
-  render() {
-    if (!this.context.isLoading) return null;
-
-    return createPortal(
-      <div className="absolute bottom-0 left-0 right-0 top-0 z-50 m-auto flex animate-fade-in items-center justify-center bg-black/20 backdrop-blur-sm">
-        <span className="loader -translate-x-5" />
-      </div>,
-      document.body,
-    );
-  }
+  return createPortal(
+    <div className="absolute bottom-0 left-0 right-0 top-0 z-50 m-auto flex animate-fade-in items-center justify-center bg-black/20 backdrop-blur-sm">
+      <span className="loader -translate-x-5" />
+    </div>,
+    document.body,
+  );
 }
 
 export default Spinner;
