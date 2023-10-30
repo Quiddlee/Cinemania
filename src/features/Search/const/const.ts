31e0855b3494bbe5b0a822ff29d6a23e0fb47b13
Bogdan <thegarrybit@gmail.com>
2023-10-30 18:01:59 +0200

    refactor: change Search provider to functional components

---
+++ src/features/Search/const/const.ts
@@ -0,0 +1,4 @@
+import { MovieList } from '../../../shared/types/types.ts';
+
+export const NO_MOVIES: MovieList = [];
+export const NO_RESULTS = 0;
--- src/features/Search/context/SearchProvider.tsx
+++ src/features/Search/context/SearchProvider.tsx
@@ -1,82 +1,116 @@
-import { Component, createContext } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useReducer,
+} from 'react';
 
 import { getMovieList } from '../../../entities/movie/api/apiMovie.ts';
 import { IChildren } from '../../../shared/types/interfaces.ts';
 import { MovieList } from '../../../shared/types/types.ts';
+import { NO_MOVIES, NO_RESULTS } from '../const/const.ts';
+import {
+  Action,
+  IInitialState,
+  ISearchContext,
+  SearchActions,
+} from '../types/types.ts';
 
-interface ISearchProviderState {
-  query: string;
-  movies: MovieList | null;
-  totalResults: number;
-  isLoading: boolean;
-}
-
-export interface ISearchContext extends ISearchProviderState {
-  updateQuery: (newQuery: string) => void;
-  updateMovies: (newMovies: MovieList, totalResults: number) => void;
-  updateIsLoading: (isLoading: boolean) => void;
-  fetchMovies: (searchQuery: string) => void;
-}
-
-const NO_RESULTS = 0;
-const NO_MOVIES: MovieList = [];
+const initialState: IInitialState = {
+  query: '',
+  movies: null,
+  totalResults: 0,
+  isLoading: false,
+};
 
 export const SearchContext = createContext<ISearchContext>({
   query: '',
 } as ISearchContext);
 
-class SearchProvider extends Component<IChildren, ISearchProviderState> {
-  state = {
-    query: '',
-    movies: null,
-    totalResults: 0,
-    isLoading: false,
-  };
+function reducer(state: IInitialState, action: Action): IInitialState {
+  switch (action.type) {
+    case SearchActions.QUERY_UPDATED:
+      return { ...state, query: action.payload };
 
-  updateQuery = (newQuery: string) => {
-    this.setState({ query: newQuery });
-  };
+    case SearchActions.MOVIES_LOADED:
+      return {
+        ...state,
+        movies: action.payload.movies,
+        totalResults: action.payload.totalResults,
+        isLoading: false,
+      };
 
-  updateMovies = (newMovies: MovieList, totalResults: number) => {
-    this.setState({ movies: newMovies, totalResults });
-  };
+    case SearchActions.LOADING:
+      return { ...state, isLoading: true };
 
-  updateIsLoading = (isLoading: boolean) => {
-    this.setState({ isLoading });
-  };
-
-  fetchMovies = async (searchQuery: string) => {
-    try {
-      this.updateIsLoading(true);
-      const res = await getMovieList(searchQuery);
-      this.updateMovies(res.Search, Number(res.totalResults));
-    } catch (e) {
-      this.updateMovies(NO_MOVIES, NO_RESULTS);
-    } finally {
-      this.updateIsLoading(false);
-    }
-  };
-
-  render() {
-    const { query, movies, totalResults, isLoading } = this.state;
-    const { updateQuery, updateMovies, updateIsLoading, fetchMovies } = this;
-
-    return (
-      <SearchContext.Provider
-        value={{
-          query,
-          movies,
-          totalResults,
-          isLoading,
-          updateQuery,
-          updateMovies,
-          updateIsLoading,
-          fetchMovies,
-        }}>
-        {this.props.children}
-      </SearchContext.Provider>
-    );
+    default:
+      throw new Error('The action does not exist!');
   }
+}
+
+function SearchProvider({ children }: IChildren) {
+  const [{ query, movies, totalResults, isLoading }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  const updateQuery = useCallback((newQuery: string) => {
+    dispatch({ type: SearchActions.QUERY_UPDATED, payload: newQuery });
+  }, []);
+
+  const updateMovies = useCallback(
+    (newMovies: MovieList, newTotalResults: number) => {
+      dispatch({
+        type: SearchActions.MOVIES_LOADED,
+        payload: {
+          movies: newMovies,
+          totalResults: newTotalResults,
+        },
+      });
+    },
+    [],
+  );
+
+  const fetchMovies = useCallback(
+    async (searchQuery: string) => {
+      try {
+        dispatch({ type: SearchActions.LOADING });
+        const res = await getMovieList(searchQuery);
+        updateMovies(res.Search, Number(res.totalResults));
+      } catch (e) {
+        updateMovies(NO_MOVIES, NO_RESULTS);
+      }
+    },
+    [updateMovies],
+  );
+
+  const providerValue = useMemo(
+    () => ({
+      query,
+      movies,
+      totalResults,
+      isLoading,
+      updateQuery,
+      fetchMovies,
+    }),
+    [fetchMovies, isLoading, movies, query, totalResults, updateQuery],
+  );
+
+  return (
+    <SearchContext.Provider value={providerValue}>
+      {children}
+    </SearchContext.Provider>
+  );
+}
+
+export function useSearch() {
+  const context = useContext(SearchContext);
+
+  if (context === undefined)
+    throw new Error('SearchContext is used outside the SearchProvider!');
+
+  return context;
 }
 
 export default SearchProvider;
