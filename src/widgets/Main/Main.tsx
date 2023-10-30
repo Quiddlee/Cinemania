e67b3df6bc67ab10e0cab621df1cede8fe5341a5
Bogdan <thegarrybit@gmail.com>
2023-10-30 18:43:39 +0200

    refactor: change main to functional component

--- src/widgets/Main/Main.tsx
+++ src/widgets/Main/Main.tsx
@@ -1,13 +1,7 @@
-import { Component } from 'react';
-
 import { IChildren } from '../../shared/types/interfaces.ts';
 
-class Main extends Component<IChildren> {
-  render() {
-    const { children } = this.props;
-
-    return <main className="relative px-6">{children}</main>;
-  }
+function Main({ children }: IChildren) {
+  return <main className="relative px-6">{children}</main>;
 }
 
 export default Main;
