6e3a50e7e9a1aed68f35b79680bc8a83fb617a12
Bogdan <thegarrybit@gmail.com>
2023-10-30 17:12:22 +0200

    refactor: change app component to functional

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,16 @@
-import { Component } from 'react';
-
 import SearchProvider from '../features/Search/context/SearchProvider.tsx';
 import MainLayout from '../pages/MainLayout.tsx';
 import ErrorBoundary from '../shared/ui/ErrorBoundary.tsx';
 import FallbackUi from '../shared/ui/FallbackUi.tsx';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary fallback={<FallbackUi />}>
-        <SearchProvider>
-          <MainLayout />
-        </SearchProvider>
-      </ErrorBoundary>
-    );
-  }
+function App() {
+  return (
+    <ErrorBoundary fallback={<FallbackUi />}>
+      <SearchProvider>
+        <MainLayout />
+      </SearchProvider>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
