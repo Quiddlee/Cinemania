da1cbed124f605e3dbd8bf0dabc16b781fe5007f
Bogdan <thegarrybit@gmail.com>
2023-10-30 19:12:34 +0200

    refactor: change not found to functional component

--- src/features/ProductList/ui/NotFound.tsx
+++ src/features/ProductList/ui/NotFound.tsx
@@ -1,19 +1,15 @@
-import { Component } from 'react';
-
 import Modal from '../../../shared/ui/Modal.tsx';
 
-class NotFound extends Component {
-  render() {
-    return (
-      <Modal>
-        <span className="text-2xl font-semibold text-gray-200">
-          No Movies Found. 🥺
-        </span>
-        <p className="mt-2">You&apos;re search did not match any movies.</p>
-        <p>Please try again. 👉👈</p>
-      </Modal>
-    );
-  }
+function NotFound() {
+  return (
+    <Modal>
+      <span className="text-2xl font-semibold text-gray-200">
+        No Movies Found. 🥺
+      </span>
+      <p className="mt-2">You&apos;re search did not match any movies.</p>
+      <p>Please try again. 👉👈</p>
+    </Modal>
+  );
 }
 
 export default NotFound;
