88627c9c0ea026bf8d669f682eb63dafcf2af5fc
Bogdan <thegarrybit@gmail.com>
2023-10-30 18:08:47 +0200

    refactor: change header to functional components

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,15 +1,11 @@
-import { Component } from 'react';
-
 import { IChildren } from '../../shared/types/interfaces.ts';
 
-class Header extends Component<IChildren> {
-  render() {
-    return (
-      <header className="sticky top-0 z-10 flex w-full flex-wrap items-center justify-between gap-x-6 gap-y-4 p-6 backdrop-blur-xl sm:gap-0 lg:h-24 lg:px-12 xl:px-20">
-        {this.props.children}
-      </header>
-    );
-  }
+function Header({ children }: IChildren) {
+  return (
+    <header className="sticky top-0 z-10 flex w-full flex-wrap items-center justify-between gap-x-6 gap-y-4 p-6 backdrop-blur-xl sm:gap-0 lg:h-24 lg:px-12 xl:px-20">
+      {children}
+    </header>
+  );
 }
 
 export default Header;
