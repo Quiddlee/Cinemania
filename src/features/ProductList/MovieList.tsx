c726d5d8d731484152b0ff3d083be4e1ed3aedf6
Bogdan <thegarrybit@gmail.com>
2023-10-30 18:48:54 +0200

    refactor: change movie list to functional component

--- src/features/ProductList/MovieList.tsx
+++ src/features/ProductList/MovieList.tsx
@@ -1,37 +1,21 @@
-import { Component } from 'react';
-
 import NotFound from './ui/NotFound.tsx';
 import Movie from '../../entities/movie/ui/Movie.tsx';
-import { MovieList as MovieListData } from '../../shared/types/types.ts';
-import {
-  ISearchContext,
-  SearchContext,
-} from '../Search/context/SearchProvider.tsx';
+import useSearch from '../Search/hooks/useSearch.ts';
 
-interface IMovieListState {
-  movies: MovieListData | null;
-}
+function MovieList() {
+  const { movies, isLoading } = useSearch();
+  const isNoMovies = !movies?.length;
 
-class MovieList extends Component<object, IMovieListState> {
-  static contextType = SearchContext;
+  if (isNoMovies && !isLoading) return <NotFound />;
+  if (isNoMovies) return null;
 
-  declare context: ISearchContext;
-
-  render() {
-    const { movies, isLoading } = this.context;
-    const isNoMovies = !movies?.length;
-
-    if (isNoMovies && !isLoading) return <NotFound />;
-    if (isNoMovies) return null;
-
-    return (
-      <ul className="m-auto flex max-w-6xl animate-fade-in flex-wrap items-center justify-center gap-6 last:m-auto sm:gap-10">
-        {movies.map((movie, i) => (
-          <Movie key={movie.imdbID} data={movie} delay={i} />
-        ))}
-      </ul>
-    );
-  }
+  return (
+    <ul className="m-auto flex max-w-6xl animate-fade-in flex-wrap items-center justify-center gap-6 last:m-auto sm:gap-10">
+      {movies.map((movie, i) => (
+        <Movie key={movie.imdbID} data={movie} delay={i} />
+      ))}
+    </ul>
+  );
 }
 
 export default MovieList;
