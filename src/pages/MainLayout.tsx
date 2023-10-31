29e8f4dbd62e3f86ad6d9f5d19f94f33c7af7193
Bogdan <thegarrybit@gmail.com>
2023-10-31 16:51:51 +0200

    refactor: remove error button

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-
 import GradientBackground from './ui/GradientBackground.tsx';
 import MovieList from '../features/ProductList/MovieList.tsx';
 import Search from '../features/Search/Search.tsx';
 import useScroll from '../shared/hooks/useScroll.ts';
-import Button from '../shared/ui/Button.tsx';
 import Spinner from '../shared/ui/Spinner.tsx';
 import Header from '../widgets/Header/Header.tsx';
 import Logo from '../widgets/Header/ui/Logo.tsx';
@@ -12,10 +9,7 @@ import TotalResults from '../widgets/Header/ui/TotalResults.tsx';
 import Main from '../widgets/Main/Main.tsx';
 
 function MainLayout() {
-  const [isError, setIsError] = useState(false);
   const containerRef = useScroll<HTMLDivElement>();
-
-  if (isError) throw new Error('test error');
 
   return (
     <>
@@ -28,12 +22,6 @@ function MainLayout() {
           <Header>
             <Logo />
             <Search />
-            <Button
-              className="animate-fade-in"
-              type="empty"
-              onClick={() => setIsError(true)}>
-              Throw error
-            </Button>
             <TotalResults />
           </Header>
           <Main>
