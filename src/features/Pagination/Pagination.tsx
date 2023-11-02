b4d53d7764611fe9823837c3b481149c4b1559cb
Bogdan <thegarrybit@gmail.com>
2023-11-02 18:47:36 +0200

    refactor: change to not show pagination on empty list

--- src/features/Pagination/Pagination.tsx
+++ src/features/Pagination/Pagination.tsx
@@ -9,11 +9,12 @@ import useSearch from '../Search/hooks/useSearch.ts';
 
 function Pagination() {
   const { setUrl, readUrl } = useUrl();
-  const { fetchMovies, query, isLoading } = useSearch();
+  const { fetchMovies, query, isLoading, totalResults } = useSearch();
 
   const currPage = Number(readUrl(PAGE_PARAM));
   const isPrevDisabled = currPage === 1;
   const isPage = Boolean(currPage);
+  const noPages = totalResults <= 10;
 
   const handleNextPage = useCallback(() => {
     const newPage = currPage + 1;
@@ -31,6 +32,8 @@ function Pagination() {
     setUrl(PAGE_PARAM, String(newPage));
     fetchMovies(query, newPage);
   }, [isPage, currPage, setUrl, fetchMovies, query]);
+
+  if (noPages) return null;
 
   return (
     <div className="pointer-events-none absolute h-screen w-screen">
