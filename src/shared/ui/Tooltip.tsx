922ff157f949cb29a9994db4599930f0dfacb5c1
Bogdan <thegarrybit@gmail.com>
2023-11-03 19:32:17 +0200

    feat: minor app improvements

--- src/shared/ui/Tooltip.tsx
+++ src/shared/ui/Tooltip.tsx
@@ -11,7 +11,7 @@ function Tooltip({ innerRef, children }: ITooltipProps) {
   return createPortal(
     <div
       ref={innerRef}
-      className="pointer-events-none invisible absolute left-0 top-0 flex h-28 w-28 scale-50 items-center justify-center rounded-full bg-lime-500 p-6 text-center text-sm font-bold text-zinc-100 opacity-0 transition-all duration-[1500ms] ease-[cubic-bezier(.13,.66,0,.95)]">
+      className="pointer-events-none invisible absolute left-0 top-0 flex h-28 w-28 scale-50 items-center justify-center rounded-full bg-lime-500 p-6 text-center text-sm font-bold text-zinc-100 opacity-0 transition-all duration-[1200ms] ease-[cubic-bezier(.13,.66,0,.95)]">
       {children}
     </div>,
     document.body,
--- src/widgets/MovieDetails/ui/BackButton.tsx
+++ src/widgets/MovieDetails/ui/BackButton.tsx
@@ -5,16 +5,16 @@ function BackButton() {
   return (
     <LinkWithQuery to="/">
       <div className="group relative flex h-10 w-10 cursor-pointer items-center justify-center overflow-hidden rounded-full py-2">
-        <div className="absolute h-full w-full rounded-full bg-neutral-300 transition-all duration-500 ease-out group-hover:scale-95" />
+        <div className="absolute h-full w-full rounded-full bg-neutral-300 transition-all duration-700 ease-out group-hover:scale-95" />
         <img
           src={chevronLeft}
           alt=""
-          className="z-10 group-hover:-translate-x-8 group-hover:opacity-0 group-hover:transition-all group-hover:duration-300 group-hover:ease-out"
+          className="z-10 group-hover:-translate-x-8 group-hover:opacity-0 group-hover:transition-all group-hover:duration-500 group-hover:ease-[cubic-bezier(.26,.81,.21,.98)]"
         />
         <img
           src={chevronLeft}
           alt=""
-          className="absolute z-10 translate-x-8 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-300 group-hover:ease-out"
+          className="absolute z-10 translate-x-8 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500 group-hover:ease-[cubic-bezier(.26,.81,.21,.98)]"
         />
       </div>
     </LinkWithQuery>
