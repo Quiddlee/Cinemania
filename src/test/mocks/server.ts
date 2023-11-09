62305af391208fc639cbf3f7206a6df72160fef5
Bogdan <thegarrybit@gmail.com>
2023-11-09 12:57:56 +0200

    fix: eslint errors

--- src/test/mocks/server.ts
+++ src/test/mocks/server.ts
@@ -1,5 +1,3 @@
-/* eslint-disable import/extensions,import/order,import/no-unresolved,import/named,import/no-duplicates,import/no-self-import,import/no-cycle,import/no-relative-packages */
-
 import { setupServer } from 'msw/node';
 
 import handlers from './handlers.ts';
