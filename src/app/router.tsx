11bf7f76a7824e9cd164b7ad3fcf74025c625044
Bogdan <thegarrybit@gmail.com>
2023-11-10 12:43:37 +0200

    refactor: add type to router constant

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,11 +1,11 @@
-import { createHashRouter } from 'react-router-dom';
+import { createHashRouter, RouteObject } from 'react-router-dom';
 
 import loader from '../entities/movie/loader.ts';
 import AppLayout from '../pages/AppLayout/AppLayout.tsx';
 import MovieDetails from '../widgets/MovieDetails/MovieDetails.tsx';
 import BackButton from '../widgets/MovieDetails/ui/BackButton.tsx';
 
-export const ROUTES = [
+export const ROUTES: RouteObject[] = [
   {
     element: <AppLayout />,
     path: '/',
