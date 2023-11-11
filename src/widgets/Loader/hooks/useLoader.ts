dad102927f576a26e413849346915405682dae62
Bogdan <thegarrybit@gmail.com>
2023-11-11 21:45:40 +0200

    feat: add JSDoc comment

--- src/widgets/Loader/hooks/useLoader.ts
+++ src/widgets/Loader/hooks/useLoader.ts
@@ -6,6 +6,12 @@ import { useNavigation } from 'react-router-dom';
 import useSearch from '../../../features/Search/hooks/useSearch.ts';
 import { LOADING_STATE } from '../../../shared/const/const.ts';
 
+/**
+ * A custom hook that checks if the loader or the search is currently loading.
+ *
+ * @param {RefObject<LocomotiveScroll>} scrollRef - The reference to the scroll component.
+ * @return {boolean} isLoading - A boolean indicating if the loader is loading.
+ */
 function useLoader(scrollRef: RefObject<LocomotiveScroll>) {
   const navigation = useNavigation();
   const { isLoading: searchIsLoading } = useSearch();
