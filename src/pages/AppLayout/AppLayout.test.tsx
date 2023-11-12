7946d6c4034150184085859cb80594b517cb83d0
Bogdan <thegarrybit@gmail.com>
2023-11-12 19:07:38 +0200

    feat: add not found page test

--- src/pages/AppLayout/AppLayout.test.tsx
+++ src/pages/AppLayout/AppLayout.test.tsx
@@ -5,10 +5,14 @@ import renderWithRouter from '../../test/helpers/RenderWithRouter.tsx';
 
 describe('App', () => {
   it('should render the app', async () => {
-    // 1. arrange
     renderWithRouter();
-    // 2. act
-    // 3. expect
+
     expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument();
+  });
+
+  it('should display 404 page when navigating to an invalid route', () => {
+    renderWithRouter(null, ['test/not-found']);
+
+    expect(screen.getByTestId('not-found')).toBeInTheDocument();
   });
 });
--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,22 +4,22 @@ import GradientBackground from '../AppLayout/ui/GradientBackground.tsx';
 
 function NotFound() {
   return (
-    <div>
-      <main className="flex h-screen items-center justify-center p-6">
-        <GradientBackground />
-        <Modal className="grid justify-items-center gap-4 px-8 py-10">
-          <h2 className="text-2xl font-semibold text-gray-200">
-            Page not found ⛔
-          </h2>
+    <main
+      data-testid="not-found"
+      className="flex h-screen items-center justify-center p-6">
+      <GradientBackground />
+      <Modal className="grid justify-items-center gap-4 px-8 py-10">
+        <h2 className="text-2xl font-semibold text-gray-200">
+          Page not found ⛔
+        </h2>
 
-          <p>We&apos;re sorry, the page you are looking for does not exist</p>
+        <p>We&apos;re sorry, the page you are looking for does not exist</p>
 
-          <LinkWithQuery to="/" className="text-blue-400 underline">
-            Go back
-          </LinkWithQuery>
-        </Modal>
-      </main>
-    </div>
+        <LinkWithQuery to="/" className="text-blue-400 underline">
+          Go back
+        </LinkWithQuery>
+      </Modal>
+    </main>
   );
 }
 
