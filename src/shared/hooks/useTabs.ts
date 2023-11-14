90fcfb31deaea6f0e829ddd235b674562aa94e30
Bogdan <thegarrybit@gmail.com>
2023-11-14 14:22:00 +0200

    fix: tab width

--- src/shared/hooks/useTabs.ts
+++ src/shared/hooks/useTabs.ts
@@ -1,20 +1,20 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { itemsPerPage } from '../types/enums.ts';
 
 function useTabs(activeValue: number, numOfTabs: number) {
-  const tabSliderWidth = useRef(0);
+  const [tabSliderWidth, setTabSliderWidth] = useState(0);
   const containerRef = useRef<HTMLDivElement>(null);
   const containerWidth = useRef(0);
   const containerPadding = useRef(6);
 
   const halfContainer = containerWidth.current / 2;
-  const halfTabsSlider = tabSliderWidth.current / 2;
+  const halfTabsSlider = tabSliderWidth / 2;
 
   const start = containerPadding.current;
   const middle = halfContainer - halfTabsSlider;
   const end =
-    containerWidth.current - tabSliderWidth.current - containerPadding.current;
+    containerWidth.current - tabSliderWidth - containerPadding.current;
 
   let position = start;
   if (activeValue === itemsPerPage.FIVE) position = middle;
@@ -22,8 +22,9 @@ function useTabs(activeValue: number, numOfTabs: number) {
 
   useEffect(() => {
     containerWidth.current = containerRef.current?.offsetWidth ?? 0;
-    tabSliderWidth.current =
-      containerWidth.current / numOfTabs - containerPadding.current;
+    setTabSliderWidth(
+      containerWidth.current / numOfTabs - containerPadding.current,
+    );
   }, [numOfTabs]);
 
   return { position, containerRef, tabSliderWidth };
