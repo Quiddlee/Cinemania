f4aac2593a64b713cefb59691f7b4eba023b979b
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-17 23:26:03 +0200

    fix: tooltip initial position

--- src/shared/ui/Tooltip.tsx
+++ src/shared/ui/Tooltip.tsx
@@ -10,6 +10,9 @@ interface ITooltipProps {
 function Tooltip({ innerRef, children }: ITooltipProps) {
   return createPortal(
     <div
+      style={{
+        translate: '1000px 1000px',
+      }}
       ref={innerRef}
       className="pointer-events-none absolute left-0 top-0 flex h-28 w-28 items-center justify-center rounded-full bg-lime-400 p-6 text-center text-sm font-bold text-zinc-950 [transition:_translate_1500ms_cubic-bezier(.08,.9,.21,.98)]">
       {children}
