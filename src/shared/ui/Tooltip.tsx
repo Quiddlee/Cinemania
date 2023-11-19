5e50cd54da1ccc4e4943013304dbfa4e2dbdb90c
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-19 14:00:38 +0200

    feat: add tooltip shadow

--- src/shared/ui/Tooltip.tsx
+++ src/shared/ui/Tooltip.tsx
@@ -14,7 +14,7 @@ function Tooltip({ innerRef, children }: ITooltipProps) {
         translate: '1000px 1000px',
       }}
       ref={innerRef}
-      className="pointer-events-none absolute left-0 top-0 flex h-28 w-28 items-center justify-center rounded-full bg-lime-400 p-6 text-center text-sm font-bold text-zinc-950 [transition:_translate_1500ms_cubic-bezier(.08,.9,.21,.98)]">
+      className="pointer-events-none absolute left-0 top-0 flex h-28 w-28 items-center justify-center rounded-full bg-lime-400 p-6 text-center text-sm font-bold text-zinc-950 shadow-md [transition:_translate_1500ms_cubic-bezier(.08,.9,.21,.98)]">
       {children}
     </div>,
     document.body,
