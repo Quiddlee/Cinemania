5c5866dd68361e9ce4da6ada018efeca3e6b8e3b
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-23 15:45:23 +0200

    refactor: remove unused App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,21 @@
 import type { AppProps } from 'next/app';
 
+import ErrorBoundary from '@shared/ui/ErrorBoundary';
+import FallbackUi from '@shared/ui/FallbackUi';
+import AppLayout from '@widgets/AppLayout/AppLayout';
+
 import ReduxProvider from '../app/model/provider';
 
 import '@styles/globals.css';
-import AppLayout from '@widgets/AppLayout/AppLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ReduxProvider>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </ReduxProvider>
+    <ErrorBoundary fallback={<FallbackUi />}>
+      <ReduxProvider>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ReduxProvider>
+    </ErrorBoundary>
   );
 }
