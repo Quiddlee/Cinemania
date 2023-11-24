968079ff0f32037de3627b0cb369727697872375
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-24 18:06:35 +0200

    feat: change tooltip font size

--- shared/ui/Tooltip.tsx
+++ shared/ui/Tooltip.tsx
@@ -1,8 +1,15 @@
 import { PropsWithChildren, RefObject } from 'react';
 
+import { Quicksand } from 'next/font/google';
+
 interface ITooltipProps extends PropsWithChildren {
   innerRef: RefObject<HTMLDivElement>;
 }
+
+const quickSand = Quicksand({
+  subsets: ['latin'],
+  weight: '700',
+});
 
 function Tooltip({ innerRef, children }: ITooltipProps) {
   return (
@@ -11,7 +18,7 @@ function Tooltip({ innerRef, children }: ITooltipProps) {
         translate: '1000px 1000px',
       }}
       ref={innerRef}
-      className="pointer-events-none absolute left-0 top-0 z-30 flex h-28 w-28 items-center justify-center rounded-full bg-lime-400 p-6 text-center text-sm font-bold text-zinc-950 shadow-md [transition:_translate_1500ms_cubic-bezier(.08,.9,.21,.98)]">
+      className={`${quickSand.className} pointer-events-none absolute left-0 top-0 z-30 flex h-28 w-28 items-center justify-center rounded-full bg-lime-400 p-6 text-center text-sm font-bold text-zinc-950 shadow-md [transition:_translate_1500ms_cubic-bezier(.08,.9,.21,.98)]`}>
       {children}
     </div>
   );
