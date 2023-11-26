931bf8a20b218ab6fa63f89f50aca2a273d31f68
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-26 17:08:25 +0200

    refactor: add width and height to Image

--- features/Pagination/ui/Button.tsx
+++ features/Pagination/ui/Button.tsx
@@ -37,11 +37,17 @@ const Button = memo(function Button({
         <div className="absolute flex h-full w-full animate-pagination-fade-in items-center justify-center rounded-full border border-lime-400/80 [transition:_background-color_0.4s_cubic-bezier(.18,.33,0,.95)_0.3s,_transform_0.3s_cubic-bezier(.18,.33,0,.95)] group-hover:scale-[0.3] group-hover:bg-lime-400 group-hover:duration-[650ms] group-active:scale-[0.25] group-active:duration-300 group-disabled:group-active:scale-[0.3]" />
         <div className="relative flex h-fit w-1/5 items-center justify-center overflow-hidden">
           <Image
+            width="0"
+            height="0"
+            sizes="100vw"
             className={`${arrowDirectionMain} z-10 text-5xl group-hover:opacity-0 group-hover:transition-all group-hover:duration-[650ms] group-hover:ease-[cubic-bezier(.18,.33,0,.95)]`}
             src={children}
             alt=""
           />
           <Image
+            width="0"
+            height="0"
+            sizes="100vw"
             className={`${arrowDirectionSecondSnooze} absolute z-10 text-5xl opacity-0 group-hover:translate-x-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-[650ms] group-hover:ease-[cubic-bezier(.18,.33,0,.95)]`}
             src={children}
             alt=""
--- widgets/MovieDetails/ui/BackButton.tsx
+++ widgets/MovieDetails/ui/BackButton.tsx
@@ -10,11 +10,17 @@ const BackButton = memo(function BackButton() {
     <LinkWithQuery data-testid="details-close" href="/">
       <div className="group relative my-auto flex h-10 w-10 cursor-pointer items-center justify-center overflow-hidden rounded-full py-2 before:absolute before:h-full before:w-full before:rounded-full before:bg-neutral-800 before:transition-all before:duration-700 before:ease-out hover:before:scale-95">
         <Image
+          width="0"
+          height="0"
+          sizes="100vw"
           src={chevronLeft}
           alt=""
           className="z-10 group-hover:-translate-x-8 group-hover:opacity-0 group-hover:transition-all group-hover:duration-500 group-hover:ease-[cubic-bezier(.26,.81,.21,.98)]"
         />
         <Image
+          width="0"
+          height="0"
+          sizes="100vw"
           src={chevronLeft}
           alt=""
           className="absolute z-10 translate-x-8 opacity-0 group-hover:translate-x-0 group-hover:opacity-100 group-hover:transition-all group-hover:duration-500 group-hover:ease-[cubic-bezier(.26,.81,.21,.98)]"
