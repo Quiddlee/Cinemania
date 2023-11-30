ff2a4513f822a015969ca32c36524c40f8e73932
Bogdan <bogdanscherbinadev@gmail.com>
2023-11-30 23:03:16 +0200

    fix: layout issue

--- src/app/ui/AppLayout.tsx
+++ src/app/ui/AppLayout.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => (
-  <main className="container m-auto h-screen max-w-6xl pt-6 font-medium">
-    <article className="flex h-full items-center justify-center gap-4">
+  <main className="container m-auto h-fit max-w-6xl py-6 font-medium">
+    <article className="flex h-full items-start justify-center gap-4">
       <Outlet />
     </article>
   </main>
