381957150f4dc9aa201387b5c29efcd13d8da01e
Bogdan <bogdanscherbinadev@gmail.com>
2023-12-02 13:43:44 +0200

    feat: implement to render the stored data on the main route

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,14 +1,51 @@
 import ROUTES from '@customTypes/enums';
+import useAppSelector from '@shared/lib/hooks/useAppSelector';
+import selectHookFormData from '@shared/lib/selectors/selectHookFormData';
 import LinkButton from '@shared/ui/LinkButton';
+import Form from '@widgets/form/Form';
+import FormRow from '@widgets/form/ui/FormRow';
 
-const Main = () => (
-  <>
-    <span>main</span>
-    <LinkButton to={`../${ROUTES.UNCONTROLLED_FORM}`}>
-      Uncontrolled form
-    </LinkButton>
-    <LinkButton to={`../${ROUTES.HOOK_FORM}`}>Hook form</LinkButton>
-  </>
-);
+const Main = () => {
+  const { formData: hookFormData } = useAppSelector(selectHookFormData);
+
+  return (
+    <div>
+      <span>main</span>
+
+      <LinkButton to={`../${ROUTES.UNCONTROLLED_FORM}`}>
+        Uncontrolled form
+      </LinkButton>
+      <LinkButton to={`../${ROUTES.HOOK_FORM}`}>Hook form</LinkButton>
+
+      <Form>
+        {hookFormData && (
+          <>
+            <FormRow label="name">
+              <span>{hookFormData.name}</span>
+            </FormRow>
+            <FormRow label="age">
+              <span>{hookFormData.age}</span>
+            </FormRow>
+            <FormRow label="email">
+              <span>{hookFormData.email}</span>
+            </FormRow>
+            <FormRow label="password">
+              <span>{hookFormData.password}</span>
+            </FormRow>
+            <FormRow label="country">
+              <span>{hookFormData.country}</span>
+            </FormRow>
+            <FormRow label="gender">
+              <span>{hookFormData.gender}</span>
+            </FormRow>
+            <FormRow label="terms and conditions">
+              <span>{hookFormData.termsAndConditions}</span>
+            </FormRow>
+          </>
+        )}
+      </Form>
+    </div>
+  );
+};
 
 export default Main;
